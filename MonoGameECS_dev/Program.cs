59982a6990c79a200dc4dfb034cad15092b30e55
Carl Johan Gribel <cjgribel@gmail.com>
2021-08-05 23:31:06 +0200

    first commit

---
+++ MonoGameECS_dev/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MonoGameECS_dev
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
